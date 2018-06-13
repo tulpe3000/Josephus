96d6aca19d1ae4cd218175c299e96a2e43090d9d
Thomas Frenzel <t.frentzel@gmx.de>
2018-06-13 08:39:33 +0200

    Projektdateien hinzufügen.

---
+++ JosephusSurvivor/Josephus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Josephus
+{
+    public class JosephusSurvivor
+    {
+        public static int JosSurvivor(int n, int k)
+        {
+            int[] survivorList = new int[n];
+            for (int i = 0; i < n; i++)
+                survivorList[i] = i + 1;
+
+           return getSurvivor(survivorList, k);
+        }
+        private static int getSurvivor(int[] survivorList, int k)
+        {
+            int length = survivorList.Length - 1;
+            if (length == 0)
+                return survivorList[0];
+                else
+            {
+                int[] newsurvivorList = new int[length];
+                for (int i = 0; i < length; i++)
+                    newsurvivorList[i] = survivorList[(i + k) % survivorList.Length];
+                return getSurvivor(newsurvivorList, k);                   
+                }
+        }
+     }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Josephus;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Tests()
+        {
+            Console.WriteLine("Basic Tests JosSurvivor");
+            Assert.AreEqual(4,JosephusSurvivor.JosSurvivor(7, 3));
+            Assert.AreEqual(10,JosephusSurvivor.JosSurvivor(11, 19));
+            Assert.AreEqual(28, JosephusSurvivor.JosSurvivor(40, 3));
+            Assert.AreEqual(13, JosephusSurvivor.JosSurvivor(14, 2));
+            Assert.AreEqual(100, JosephusSurvivor.JosSurvivor(100, 1));
+            Assert.AreEqual(1, JosephusSurvivor.JosSurvivor(1, 300));
+            Assert.AreEqual(1, JosephusSurvivor.JosSurvivor(2, 300));
+            Assert.AreEqual(1, JosephusSurvivor.JosSurvivor(5, 300));
+            Assert.AreEqual(7, JosephusSurvivor.JosSurvivor(7, 300));
+            Assert.AreEqual(265, JosephusSurvivor.JosSurvivor(300, 300));
+        }
+    }
+}
